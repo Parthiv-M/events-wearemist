f988831ec5cc330dfe7f30d8b9972622ce514e93
Parthiv-M <parthiv.menon@learner.manipal.edu>
2021-04-16 20:14:31 +0530

    fixed data fetching from json

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,40 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const getEventsData = async() => {
-  const eventsData = await fetch('https://raw.githubusercontent.com/Parthiv-M/events-wearemist/parthiv/data/events.json');
-  const parsedEvents = await eventsData.json();
-  console.log(parsedEvents);
-  return parsedEvents;
-}
-
-const renderEvents = () => {
-  const data = getEventsData();
+const RenderEvents = ({data}) => {
   return (
     <div>
-      <p>{data}</p>
+        {
+        data.map((event, index) => (
+          <ul>
+            <li key={index}>
+              {event.text}
+            </li>
+          </ul>
+        ))
+        }
     </div>
   );
 }
 
 function App() {
+  
+  const getData = async () => {
+    const eventsData = await fetch('https://raw.githubusercontent.com/Parthiv-M/events-wearemist/parthiv/data/events.json');
+    const parsedEvents = await eventsData.json();    
+    setEventsData(parsedEvents);
+  }
+
+  const [data, setEventsData] = useState([]);
+
+  useEffect(() => {  
+      getData();
+  }, []);
+
   return (
     <div className="App">
       <h3>Hello</h3>
-      {renderEvents}
+      <RenderEvents data={data}/>
     </div>
   );
 }
