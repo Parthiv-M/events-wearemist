316a875a7dd204606a67d9c31e7a4cba759808e0
Parthiv-M <parthiv.menon@learner.manipal.edu>
2021-04-16 19:42:03 +0530

    testing data fetcher

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 
+const getEventsData = async() => {
+  const eventsData = await fetch('https://raw.githubusercontent.com/Parthiv-M/events-wearemist/parthiv/data/events.json');
+  const parsedEvents = await eventsData.json();
+  console.log(parsedEvents);
+  return parsedEvents;
+}
+
+const renderEvents = () => {
+  const data = getEventsData();
+  return (
+    <div>
+      <p>{data}</p>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <h3>Hello</h3>
+      {renderEvents}
     </div>
   );
 }
