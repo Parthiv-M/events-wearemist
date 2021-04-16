3955baeb301138fdcda39bc59587a8a1c7f145a0
PalChheda <chhedapal@yahoo.in>
2021-04-17 00:09:24 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
+import EventsCards from './Components/card';
 
 import EventsCarousel from './Components/carousel';
 
 function App() {
   return (
     <div className="App">
-      <h3>Hello</h3>
-      <h1>hi</h1>
+      <h1>Hello</h1>
+      <h2>hi</h2>
       <EventsCarousel/>
+      <br/>
+      <h1>Past Events</h1>
+      <br/>
+      <br/>
+      <EventsCards/>
     </div>
   );
 }
