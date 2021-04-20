e7a38687e62077d0b3e118eda1ce712cacfe39aa
PalChheda <chhedapal@yahoo.in>
2021-04-20 17:19:23 +0530

    Update credits.js

--- client/src/Components/credits.js
+++ client/src/Components/credits.js
@@ -1,8 +1,9 @@
 import Overlay from 'react-bootstrap/Overlay'
-import { useState, useRef} from 'react';
+import { useState, useRef} from 'react'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
-
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function Example() {
     const [show, setShow] = useState(false);
@@ -32,15 +33,20 @@ function Example() {
       ];
       const card = items.map((item) => {
           return(
-            <Card style={{ width: '18rem', height:'70vh', margin:'auto' }}>
-                <div style={{padding:'30px'}}>
-                    <Card.Img style={{borderRadius:'50%'}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLAFXK2MihEQSj_Udwnn1-lH6BDzU8cjq2JA&usqp=CAU" />
+            <Card style={{ width: '20rem', height:'60vh', margin:'auto', color:'white', background:'transparent', border:'none' }}>
+                <div style={{ margin:'auto'}}>
+                    <Card.Img style={{borderRadius:'50%', width:'70%', display:'block'}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLAFXK2MihEQSj_Udwnn1-lH6BDzU8cjq2JA&usqp=CAU" />
                 </div>
                 <Card.Body>
-                    <Card.Title><h1>{item.name}</h1></Card.Title>
+                    <Card.Title>
+                        <div style={{textAlign:'center', fontStyle:'bold', fontSize:'30px'}}>{item.name}</div>
+                        <div style={{textAlign:'center', fontSize:'18px'}}>{item.profession}</div>
+                        <hr style={{color:'white', backgroundColor:'white', height:'2px'}}/>
+                    </Card.Title>
                     <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
+                        <div style={{textAlign:'center', fontSize:'15px'}}>
+                            {item.description}
+                        </div>
                     </Card.Text>
                 </Card.Body>
             </Card>
@@ -57,18 +63,22 @@ function Example() {
                 {...props}
                 style={{
                     backgroundColor: 'rgba( 0, 0, 0, 0.75)',
-                    padding: '100px 20px',
+                    padding: '10px 20px',
                     margin: 'auto',
                     height:'auto',
-                    width:'95vw',
-                    position:'static',
+                    width:'100vw',
+                    position:'fixed',
+                    placement:'left-start',
                     color: 'white',
+                    zIndex:'1',
                     borderRadius: 3,
                     ...props.style,
                 }}
                 >
-                    <div style={{display:'inline'}}>
-                        {card}
+                    <div style={{display:'inline', colour:'white'}}>
+                        <Row className='p-md-5 align-items-center justify-content-center'>
+                            {card}
+                        </Row>
                     </div>
                 </div>
             )}
