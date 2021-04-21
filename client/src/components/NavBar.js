6ea3277e91ee325de0280b21e02eb1f2a2c0470d
PalChheda <chhedapal@yahoo.in>
2021-04-21 13:19:17 +0530

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './../App.css';
 import logo from './../logoLight.png';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 
 // navbar component for the page
 const EventsNavBar = () => {
@@ -16,6 +17,9 @@ const EventsNavBar = () => {
               className="d-inline-block align-top"
             />
           </Navbar.Brand>
+          <Button className = 'btn' style={{color:'white', background:'transparent', border:'none', fontSize:'1.5rem', fontWeight:'bold'}} >
+            E V E N T S
+          </Button>
         </Navbar>
     );
 }
