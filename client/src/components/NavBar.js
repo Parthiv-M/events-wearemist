edc8388534d77fc07c0eceae1305769332b27718
Parthiv-M <parthiv.menon@learner.manipal.edu>
2021-04-24 15:53:07 +0530

    favicon, navbar text  and content text update

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './../App.css';
 import logo from './../logoLight.png';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import { useEffect } from 'react';
 
 // navbar component for the page
 const EventsNavBar = () => {
@@ -18,7 +16,7 @@ const EventsNavBar = () => {
               className="d-inline-block align-top"
             />
           </Navbar.Brand>
-          <div style={{color:'white', background:'transparent', border:'none', fontSize:'1.5rem', fontWeight:'bold', cursor:'default'}} >
+          <div className="text-white" style={{ background:'transparent', fontSize:'1.5rem', fontWeight:'bold', cursor:'default' }}>
             E V E N T S
           </div>
         </Navbar>
