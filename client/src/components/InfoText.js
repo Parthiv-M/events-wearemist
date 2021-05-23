13b6901db604190e50c4994d8388105efb75b08c
Parthiv-M <parthiv.menon@learner.manipal.edu>
2021-05-23 14:39:12 +0530

    minor fix

--- client/src/components/InfoText.js
+++ client/src/components/InfoText.js
@@ -13,7 +13,7 @@ const InfoText = () => {
                     <div style={{ flexGrow: 1, borderBottom: '4px solid #3c3c3c' }}></div>
                 </div>
                 <br />
-                    This page kees a tab of all the events Manipal Information Security Team conducts for cybersecurity 
+                    This page keeps a tab of all the events Manipal Information Security Team conducts for cybersecurity 
                     enthusiasts and technology nerds. <span style={{color: 'gray'}}> Keep track of our events to stay abreast 
                     with the latest in the cyber world </span>            
             </Col>
